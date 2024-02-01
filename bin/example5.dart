a504e5bd82fef9990717d00e9938bee5d684251c
Coffeer <israel.mail@gmail.com>
2024-01-31 22:10:32 -0700

    New examples added

---
+++ bin/example5.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  final age = 10;
+
+  const age1 = 11;
+
+// Finals can be assigned a constant value
+  final age2 = age1;
+
+// Constants cannot be assigned a non-constant value,
+// so the following is an invalid operation
+  const ag3 = age;
+}
